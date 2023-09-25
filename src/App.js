300c1a85bfb0f8b607c6ff35c04fc8e8f930676b
vijaykrishna <vijaykrishna.top@gmail.com>
2023-09-26 00:34:42 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import React, { useState } from "react";
 import TextForm from './components/text';
 import NavBar from './components/navbar';
-
+import Alert from './components/Alert';
 
 function App() {
+const  [alert, setAlert]=useState(null)
+const showAlert=(messege, type)=>{
+  setAlert({
+    msg:messege,
+    type:type
+  })
+}
+  const [mode , setMode]=useState("light");
+  const toggleMode = () => {
+    const newMode = mode === "dark" ? "light" : "dark";
+    showAlert(`${newMode==='dark'?'Dark':'Light'} mode has been enable`,"success")
+    // console.log(mode);
+    // console.log(newMode);
+    setMode(newMode);
+    document.body.style.backgroundColor = mode === "dark" ? "white" : "black";
+  };
+  
+  
   return (
     <>
-   <NavBar title="Play ground"/>
-   <TextForm heading="hello"/>
+      <NavBar title="Text Utile" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
+        <div className="container my-3">
+        <TextForm heading="Hello" mode={mode} />
+        </div>
     </>
   );
 }
--- src/components/text.js
+++ src/components/text.js
@@ -1,27 +1,52 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
+export default function TextForm(props) {
+  const [text, setText] = useState("enter text here");
 
-export default function TextForm(props){
-    const [text,setText]=useState("enter text here")
-    const updateValue=(e)=>{
-        setText(e.target.value);
-    }
-    const toUpperCase=()=>{
-        setText(text.toUpperCase());
-    }
-    const toLowerCase=()=>{
-        setText(text.toLowerCase());
-    }
-    return(
-        <div className="form-control border-0">
-            <h1>{props.heading}</h1>
-            <p>{text.split(" ").length} words and {text.length} Characters</p>
-            <p>Read in {0.008 * text.split(" ").length} mins</p>
-            <div className="mb-3">
-                <textarea id="myBox" cols="20" rows="5" value={text } onChange={updateValue} className="form-control"></textarea>
-            </div>
-            <button className="btn btn-primary mx-3" onClick={toUpperCase} >convert to UpperCase</button>
-            <button className="btn btn-primary mx-3" onClick={toLowerCase}>convert to LowerCase</button>
-        </div>
-    )
+  const updateValue = (e) => {
+    setText(e.target.value);
+  };
+
+  const toUpperCase = () => {
+    setText(text.toUpperCase());
+  };
+
+  const toLowerCase = () => {
+    setText(text.toLowerCase());
+  };
+
+  const clearText = () => {
+    let text = document.getElementById("myBox");
+    text.value = "";
+  };
+
+  return (
+    <>
+      <h1 style={{ color: props.mode === "dark" ? "white" : "black" }}>{props.heading}</h1>
+      <p style={{ color: props.mode === "dark" ? "white" : "black" }}>
+        {text.split(" ").length} words and {text.length} Characters
+      </p>
+      <p style={{ color: props.mode === "dark" ? "white" : "black" }}>Read in {0.008 * text.split(" ").length} mins</p>
+      <div className="mb-3" style={{ color: props.mode === "dark" ? "white" : "black" }}>
+        <textarea
+          id="myBox"
+          cols="20"
+          rows="5"
+          value={text}
+          onChange={updateValue}
+          className="form-control"
+          style={{ color: props.mode === "dark" ? "white" : "black", backgroundColor:props.mode==="dark"?"black":"white"}}
+        ></textarea>
+      </div>
+      <button className="btn btn-primary mx-3" onClick={toUpperCase}>
+        convert to UpperCase
+      </button>
+      <button className="btn btn-primary mx-3" onClick={toLowerCase}>
+        convert to LowerCase
+      </button>
+      <button className="btn btn-primary mx-3" onClick={clearText}>
+        Clear Text
+      </button>
+    </>
+  );
 }
